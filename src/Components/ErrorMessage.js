dfd896c909916b84e0e7779029b4b78957a21679
Just-Hasan-Desktop <109896294+Just-Hasan@users.noreply.github.com>
2023-12-17 20:45:53 +0700

    Update ErrorMessage.js

--- src/Components/ErrorMessage.js
+++ src/Components/ErrorMessage.js
@@ -1,6 +1,6 @@
 export function ErrorMessage({ message }) {
   return (
-    <p className="self-center text-2xl font-black col-span-full text-accent">
+    <p className="self-center grid place-content-center rounded-xl mb-12 text-2xl font-black col-span-full bg-tailwindColorGray h-[300px] text-accent">
       {message}
     </p>
   );
